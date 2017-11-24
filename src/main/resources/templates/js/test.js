03fa730bbde72b4af02a8eb5a654d1ba52a20cc3
yangtianle <2752555817@qq.com>
2017-11-24 15:54:58 +0800

    dicias

--- src/main/resources/templates/js/test.js
+++ src/main/resources/templates/js/test.js
@@ -1,7 +1,7 @@
 /**
  * Created by 你 楠哥 。 on 2017/11/24.
  */
-window.onload = test1();
+window.onload = test2();
 
 function  test(name,age) {
     this.name = name;
@@ -13,4 +13,13 @@ function  test(name,age) {
 
 function test1() {
     new test("宋文迪",10).showSelf();
+}
+
+function test2() {
+    Date.prototype.setHello = function () {
+        alert("asdd");
+    }
+
+    var date = new Date();
+    date.setHello();
 }
